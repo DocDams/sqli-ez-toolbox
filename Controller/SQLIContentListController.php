b313f694fa2b7e0c770b1f820a09b40207867239
daverner <daverner@sqli.com>
2021-08-04 12:22:51 +0200

    [#24] Listing Content types + export csv (review)
    Signed-off-by: daverner <daverner@sqli.com>

--- Controller/SQLIContentListController.php
+++ Controller/SQLIContentListController.php
@@ -6,49 +6,41 @@ namespace SQLI\EzToolboxBundle\Controller;
 use eZ\Publish\API\Repository\ContentTypeService;
 use eZ\Publish\API\Repository\Exceptions\InvalidArgumentException;
 use eZ\Publish\API\Repository\Exceptions\NotFoundException;
-use eZ\Publish\API\Repository\Exceptions\UnauthorizedException;
-use eZ\Publish\API\Repository\FieldTypeService;
-use eZ\Publish\API\Repository\Repository;
 use eZ\Publish\API\Repository\SearchService;
+use eZ\Publish\API\Repository\Values\Content\Content;
 use eZ\Publish\API\Repository\Values\Content\LocationQuery;
 use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
-use eZ\Publish\API\Repository\Values\ValueObject;
 use eZ\Publish\Core\Pagination\Pagerfanta\LocationSearchAdapter;
-use eZ\Publish\Core\Pagination\Pagerfanta\LocationSearchHitAdapter;
 use eZ\Publish\Core\Pagination\Pagerfanta\Pagerfanta;
 use EzSystems\EzPlatformAdminUiBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\File\Stream;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class SQLIContentListController extends Controller
 {
+    /** @var SearchService */
     private $searchService;
+    /** @var ContentTypeService */
     private $contentTypeService;
-    private $fieldTypeService;
-    private $repository;
 
     /**
      * SQLIContentListController constructor.
-     * @param $searchService
-     * @param $contentTypeService
+     * @param SearchService $searchService
+     * @param ContentTypeService $contentTypeService
      */
     public function __construct(
         SearchService $searchService,
-        ContentTypeService $contentTypeService,
-        Repository $repository,
-        FieldTypeService $fieldTypeService
+        ContentTypeService $contentTypeService
     ) {
         $this->searchService = $searchService;
         $this->contentTypeService = $contentTypeService;
-        $this->repository = $repository;
-        $this->fieldTypeService = $fieldTypeService;
     }
 
     /**
      * @param bool $contentTypeIdentifier
      * @param int $page
      * @return Response
-     * @throws InvalidArgumentException
      */
     public function listAction($contentTypeIdentifier = false, int $page = 1)
     {
@@ -58,7 +50,7 @@ class SQLIContentListController extends Controller
             new Criterion\Visibility(Criterion\Visibility::VISIBLE),
         ];
 
-        if ($contentTypeIdentifier) {
+        if ($contentTypeIdentifier !== "false") {
             $criterions[] = new Criterion\ContentTypeIdentifier($contentTypeIdentifier);
         }
 
@@ -80,110 +72,59 @@ class SQLIContentListController extends Controller
             '@SQLIEzToolbox/ContentList/all_content_list.html.twig',
             [
                 'totalCount' => $paginator->getNbResults(),
-                'articles' => $paginator,
+                'paginator' => $paginator,
                 'contentTypes' => $contentTypes,
+                'selectedContentType' => $contentTypeIdentifier,
             ]
         );
     }
 
     /**
      *
-     */
-    public function getPaginator($contentTypeIdentifier, $maxPerPage = 8)
-    {
-        $query = new LocationQuery();
-
-        $criterions = [
-            new Criterion\Visibility(Criterion\Visibility::VISIBLE),
-        ];
-
-        if ($contentTypeIdentifier) {
-            $criterions[] = new Criterion\ContentTypeIdentifier($contentTypeIdentifier);
-        }
-
-        $query->query = new Criterion\LogicalAnd($criterions);
-
-        $paginator = new Pagerfanta(
-            new LocationSearchAdapter($query, $this->searchService)
-        );
-
-        if ($maxPerPage !== false) {
-            $paginator->setMaxPerPage(8);
-        }
-        return $paginator;
-    }
-
-    /**
-     *
-     * @throws NotFoundException
-     * @throws UnauthorizedException
+     * @param $contentTypeIdentifier
+     * @return BinaryFileResponse
      * @throws InvalidArgumentException
+     * @throws NotFoundException
      */
-    public function exportCSV(
-        $contentTypeIdentifier,
-        ContentTypeService $contentTypeService,
-        FieldTypeService $fieldTypeService
-    ) {
-        $response = new StreamedResponse();
-
+    public function exportCSV($contentTypeIdentifier): BinaryFileResponse
+    {
+        $filename = uniqid('/tmp/sqlitoolbox_') . ".csv";
         $query = new LocationQuery();
         $query->filter = new Criterion\ContentTypeIdentifier($contentTypeIdentifier);
         $result = $this->searchService->findContent($query, ['useAlwaysAvailable' => true]);
 
-        $response->setCallback(
-            function () use ($contentTypeService, $contentTypeIdentifier, $fieldTypeService, $result) {
-                $fp = fopen('php://output', 'w');
+        $fp = fopen($filename, 'w');
 
-                // Build Headers
-                $headers = [];
-                $contentType = $contentTypeService->loadContentTypeByIdentifier($contentTypeIdentifier);
-                foreach ($contentType->fieldDefinitions as $fieldDefinition) {
-                    array_push($headers, $fieldDefinition->identifier);
-                }
+        // Build Headers
+        $headers = [];
+        $contentType = $this->contentTypeService->loadContentTypeByIdentifier($contentTypeIdentifier);
+        foreach ($contentType->fieldDefinitions as $fieldDefinition) {
+            array_push($headers, $fieldDefinition->identifier);
+        }
 
-                // Builder Columns
-                $contents = array();
-                foreach ($result->searchHits as $searchHit) {
-                    array_push(
-                        $contents,
-                        $searchHit->valueObject
-                    );
-                }
+        fputcsv($fp, $headers, ';');
 
-                fputcsv($fp, $headers);
-
-                $columns = [];
-                foreach ($contents as $content) {
-                    $column = [];
-                    foreach ($contentType->fieldDefinitions as $fieldDefinition) {
-                        $fieldType = $fieldTypeService->getFieldType($fieldDefinition->fieldTypeIdentifier);
-                        $field = $content->getField($fieldDefinition->identifier);
-                        $valueHash = $fieldType->toHash($field->value);
-                        if (is_array($valueHash)) {
-                            $result = "";
-                            array_walk_recursive(
-                                $valueHash,
-                                function ($elt) use (&$result) {
-                                    $result .= strval($elt) . "\n";
-                                }
-                            );
-                            array_push($column, $result);
-                        } else {
-                            array_push($column, $valueHash);
-                        }
-                    }
-                    array_push($columns, $column);
-                    fputcsv($fp, array_values($column), ';', '"');
-                }
-
-                fclose($fp);
+        foreach ($result->searchHits as $searchHit) {
+            /** @var Content $content */
+            $content = $searchHit->valueObject;
+            $row = [];
+            foreach ($contentType->fieldDefinitions as $fieldDefinition) {
+                $field = $content->getFieldValue($fieldDefinition->identifier);
+                $fieldValue = $field->__toString();
+                $row[] = $fieldValue;
             }
-        );
+            fputcsv($fp, $row, ';');
+        }
 
-        $filename = 'Content_List' . $contentTypeIdentifier;
+        fclose($fp);
+        $stream = new Stream($filename);
+        $response = new BinaryFileResponse($stream);
+
         $response->headers->set('Content-Type', 'text/csv; charset=utf-8');
-        $response->headers->set('Content-Disposition', "attachment; filename=\"export-{$filename}.csv\"");
-        $response->setStatusCode(200);
+        $response->headers->set(
+            'Content-Disposition',
+            "attachment; filename=\"export-{$contentTypeIdentifier}-Contents.csv\""
+        );
         return $response;
     }
 }
