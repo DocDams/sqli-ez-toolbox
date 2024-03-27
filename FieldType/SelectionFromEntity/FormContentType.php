fe3b9d3e102a053901be5bc2bdbf517faab68c0f
abouaki <abouaki@sqli.com>
2024-03-27 10:27:37 +0100

    modif structure formContentType

---
+++ FieldType/SelectionFromEntity/FormContentType.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SQLI\EzToolboxBundle\FieldType\SelectionFromEntity;
+
+
+use Ibexa\Contracts\Core\FieldType\Value as ValueInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+final class FormContentType implements ValueInterface
+{
+
+    #[Assert\NotBlank]
+    private string $className;
+    #[Assert\NotBlank]
+    private string $valueAttribute;
+    #[Assert\NotBlank]
+    private string $labelAttribute;
+    private string $filer;
+    /**
+     * @return mixed
+     */
+    public function getClassName()
+    {
+        return $this->className;
+    }
+
+    /**
+     * @param mixed $className
+     */
+    public function setClassName($className): void
+    {
+        $this->className = $className;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValueAttribute()
+    {
+        return $this->valueAttribute;
+    }
+
+    /**
+     * @param mixed $valueAttribute
+     */
+    public function setValueAttribute($valueAttribute): void
+    {
+        $this->valueAttribute = $valueAttribute;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLabelAttribute()
+    {
+        return $this->labelAttribute;
+    }
+
+    /**
+     * @param mixed $labelAttribute
+     */
+    public function setLabelAttribute($labelAttribute): void
+    {
+        $this->labelAttribute = $labelAttribute;
+    }
+
+    public function __toString()
+    {
+        return implode(',', $this->selection);
+    }
+}
