c47026868d75464784c50009b80c473b4f069f9b
abouaki <abouaki@sqli.com>
2024-03-29 17:29:15 +0100

    template content edit de selection statique et dynamique

--- ScriptTest/script.php
+++ ScriptTest/script.php
@@ -4,7 +4,6 @@ $configSource = __DIR__ . '/Ressources/config/ez_field_templates.yml';
 
 // Chemin vers le répertoire de destination où vous souhaitez copier le fichier
 $configDestination = __DIR__ . '/../../../config/packages/app/test.yaml';
-
 // Vérifie si le fichier de destination existe déjà
 if (!file_exists($configDestination)) {
     // Copie le fichier depuis le répertoire source vers le répertoire de destination
